6e4ffbe0cd71a1382ff5ee7d6fd1c1f96bc67fe1
aigles1 <draekhen@gmail.com>
2024-08-29 22:47:05 -0400

    Update PSmenupingtester.ps1
    fixed indentations

--- PSmenupingtester.ps1
+++ PSmenupingtester.ps1
@@ -7,10 +7,10 @@ function Show-Menu {
     Write-Host "Press '1' to Ping 8.8.8.8 Google DNS"
     Write-Host "Press '2' to Ping 1.1.1.1 Cloudflare DNS"
     Write-Host "Press '3' to Ping 172.64.34.227 Cloudflare DNS"
-	Write-Host "Press '4' to Ping 205.251.196.120 Amazon DNS"
-	Write-Host "Press '5' to Ping 208.67.222.222 Cisco OpenDNS"
-	Write-Host "Press '6' to Ping 9.9.9.9 Quad9"
-	Write-Host "Press '7' to Enter a host (Example: www.cloudflare.com or 8.8.4.4)"
+    Write-Host "Press '4' to Ping 205.251.196.120 Amazon DNS"
+    Write-Host "Press '5' to Ping 208.67.222.222 Cisco OpenDNS"
+    Write-Host "Press '6' to Ping 9.9.9.9 Quad9"
+    Write-Host "Press '7' to Enter a host (Example: www.cloudflare.com or 8.8.4.4)"
     Write-Host "Press 'q' to quit"
 }
 
@@ -38,10 +38,10 @@ do {
         '1' {Invoke-Expression "Test-Connection -TargetName 8.8.8.8 -Count $count" | Format-Table}
         '2' {Invoke-Expression "Test-Connection -TargetName 1.1.1.1 -Count $count" | Format-Table}
         '3' {Invoke-Expression "Test-Connection -TargetName 172.64.34.227 -Count $count" | Format-Table}
-		'4' {Invoke-Expression "Test-Connection -TargetName 205.251.196.120 -Count $count" | Format-Table}
-		'5' {Invoke-Expression "Test-Connection -TargetName 208.67.222.222 -Count $count" | Format-Table}
-		'6' {Invoke-Expression "Test-Connection -TargetName 9.9.9.9 -Count $count" | Format-Table}
-		'7' {$Server = Read-Host -Prompt 'Input host'; Invoke-Expression "Test-Connection -TargetName $Server -Count $count" | Format-Table}
+	'4' {Invoke-Expression "Test-Connection -TargetName 205.251.196.120 -Count $count" | Format-Table}
+	'5' {Invoke-Expression "Test-Connection -TargetName 208.67.222.222 -Count $count" | Format-Table}
+	'6' {Invoke-Expression "Test-Connection -TargetName 9.9.9.9 -Count $count" | Format-Table}
+	'7' {$Server = Read-Host -Prompt 'Input host'; Invoke-Expression "Test-Connection -TargetName $Server -Count $count" | Format-Table}
         default { Write-Host "Invalid option, please try again." }
     }
 
